b46103a7e2477e263ad376517ac52cf09f22da76
stevanbrc96 <stevanbrc@gmail.com>
2025-06-09 22:05:10 +0200

    Make source code fully match verified Etherscan version (including imports).

--- dao-nft-project/contracts/DAO.sol
+++ dao-nft-project/contracts/DAO.sol
@@ -1,31 +1,26 @@
-// FILE: DAO.sol
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./MembershipNFT.sol"; 
+import "./MembershipNFT.sol";
 
 contract DAO {
-    enum VotingChoice { For, Against, Abstain }
-
     struct Proposal {
         address proposalCreator;
         string description;
         uint256 deadline;
         uint256 votesFor;
         uint256 votesAgainst;
-        uint256 votesAbstain;
         bool executed;
         mapping(address => bool) hasVoted;
     }
 
     MembershipNFT public membershipNFT;
     uint256 public proposalCount;
-    mapping(uint256 => Proposal) private proposals;
+    mapping(uint256 => Proposal) public proposals;
 
     event ProposalCreated(uint256 proposalId, address indexed creator, string description);
-    event UserVoted(uint256 proposalId, address indexed voter, VotingChoice choice);
-    event ProposalExecuted(uint256 proposalId, bool passed);
+    event UserVoted(uint256 proposalId, address indexed voter, bool support);
+    event ProposalExecuted(uint256 proposalId);
 
     error NotNFTHolder();
     error AlreadyVoted();
@@ -33,11 +28,9 @@ contract DAO {
     error VotingPeriodEnded();
     error ProposalAlreadyExecuted();
 
-    constructor(string memory _baseTokenURI, address _daoCreator) payable {
+    constructor(string memory _baseTokenURI) payable {
         membershipNFT = new MembershipNFT(_baseTokenURI);
-        membershipNFT.transferOwnership(address(this)); 
-
-        membershipNFT.mintInitialMembership(_daoCreator); 
+        membershipNFT.transferOwnership(address(this));
     }
 
     modifier onlyNFTHolder() {
@@ -51,10 +44,11 @@ contract DAO {
         p.proposalCreator = msg.sender;
         p.description = _description;
         p.deadline = block.timestamp + 7 days;
+
         emit ProposalCreated(proposalCount, msg.sender, _description);
     }
 
-    function voteForProposal(uint256 _proposalId, VotingChoice choice) external onlyNFTHolder {
+    function voteForProposal(uint256 _proposalId, bool support) external onlyNFTHolder {
         Proposal storage p = proposals[_proposalId];
         if (p.proposalCreator == address(0)) revert ProposalNotFound();
         if (p.deadline < block.timestamp) revert VotingPeriodEnded();
@@ -62,20 +56,13 @@ contract DAO {
 
         p.hasVoted[msg.sender] = true;
 
-        if (choice == VotingChoice.For) {
+        if (support) {
             p.votesFor++;
-        } else if (choice == VotingChoice.Against) {
+        } else {
             p.votesAgainst++;
-        } else if (choice == VotingChoice.Abstain) {
-            p.votesAbstain++;
         }
 
-        emit UserVoted(_proposalId, msg.sender, choice);
-    }
-
-    function proposalPassed(uint256 _proposalId) public view returns (bool) {
-        Proposal storage p = proposals[_proposalId];
-        return p.votesFor > (p.votesAgainst + p.votesAbstain);
+        emit UserVoted(_proposalId, msg.sender, support);
     }
 
     function executeProposal(uint256 _proposalId) external {
@@ -85,31 +72,7 @@ contract DAO {
         if (p.executed) revert ProposalAlreadyExecuted();
 
         p.executed = true;
-        bool passed = proposalPassed(_proposalId);
 
-        emit ProposalExecuted(_proposalId, passed);
-    }
-
-    function getProposal(uint256 _proposalId)
-        external
-        view
-        returns (
-            address proposalCreator,
-            string memory description,
-            uint256 deadline,
-            uint256 votesFor,
-            uint256 votesAgainst,
-            uint256 votesAbstain,
-            bool executed
-        )
-    {
-        Proposal storage p = proposals[_proposalId];
-        proposalCreator = p.proposalCreator;
-        description = p.description;
-        deadline = p.deadline;
-        votesFor = p.votesFor;
-        votesAgainst = p.votesAgainst;
-        votesAbstain = p.votesAbstain;
-        executed = p.executed;
+        emit ProposalExecuted(_proposalId);
     }
 }
