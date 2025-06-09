ff0f60e315eeaa1d2c395bb62ff490cca719c669
stevanbrc96 <stevanbrc@gmail.com>
2025-06-09 21:33:41 +0200

    feat: Implement auto-minting of first NFT to DAO creator

--- dao-nft-project/contracts/MembershipNFT.sol
+++ dao-nft-project/contracts/MembershipNFT.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/token/ERC721/ERC721.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MembershipNFT is ERC721, Ownable {
     uint256 public nextTokenId;
@@ -12,11 +12,19 @@ contract MembershipNFT is ERC721, Ownable {
 
     event MembershipMinted(address indexed to, uint256 tokenId);
 
-   constructor(string memory _baseTokenURI) ERC721("DAO Membership", "DAO") Ownable() {
-    nextTokenId = 1;
-    baseTokenURI = _baseTokenURI;
-}
+    constructor(string memory _baseTokenURI, address _initialRecipient) 
+        ERC721("DAO Membership", "DAO") 
+        Ownable(msg.sender) 
+    {
+        nextTokenId = 1;
+        baseTokenURI = _baseTokenURI;
 
+        _safeMint(_initialRecipient, nextTokenId);
+        hasMinted[_initialRecipient] = true;
+        nextTokenId++;
+        
+        emit MembershipMinted(_initialRecipient, nextTokenId - 1);
+    }
 
     function mint() external payable {
         require(!hasMinted[msg.sender], "Already minted");
@@ -33,5 +41,9 @@ contract MembershipNFT is ERC721, Ownable {
 
     function _baseURI() internal view override returns (string memory) {
         return baseTokenURI;
+    }
+
+    function withdraw(address payable _to) external onlyOwner {
+        _to.transfer(address(this).balance);
     }
 }
